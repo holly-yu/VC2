5ed2b0a2ac3393de6b7453378a6faf8b88dac8ca
coderyu <657379242@qq.com>
2021-05-07 10:27:32 +0800

    lisisi's

---
+++ data_process/README.md
@@ -0,0 +1,3 @@
+1. 切到该目录下运行命令 `npm install`
+2. 运行命令`node stay_points.js`
+
---
+++ data_process/stay_points.js
@@ -0,0 +1,41 @@
+const csv = require('csvtojson')
+const d3 = require('d3-array')
+const fs = require('fs'),path = require('path');
+
+const converter = csv()
+  .fromFile('./gps.csv')
+  .then((json) => {
+    let data = d3.sort(json, d=>+d.id, d=>new Date(d.Timestamp))
+
+    let car_track = d3.groups(data, d => +d.id)
+    let sample_tracks = [];
+
+    car_track.map(car=>{
+      let id = car[0];
+      let tracks = car[1];
+      let stay_points = [];
+      tracks.map((track,i)=>{
+        if(i){
+          let prev_date = new Date(tracks[i-1].Timestamp);
+          if(new Date(track.Timestamp) - prev_date > 60*1000){
+            stay_points.push({
+              Timestamp:tracks[i-1].Timestamp,
+              lat: tracks[i-1].lat,
+              long: tracks[i-1].long
+            });
+          }
+        }
+      })
+      sample_tracks.push({
+        key: id,
+        stay_points: stay_points
+      })
+  })
+  fs.writeFile('stay_points.json', JSON.stringify(sample_tracks), 'utf8', (err)=>{
+    if(err) throw err;
+    console.log('done')
+  })
+})
+
+
+
